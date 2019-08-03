20f8ea809dd4093d3a92ce5de2eccd280f1afe92
AnuragTiwari1 <anuragkumartiwari79@gmail.com>
2019-08-03 11:40:39 +0530

    swithing workspace

--- src/api/baseurl.js
+++ src/api/baseurl.js
@@ -1,4 +1,4 @@
 // @flow
-// export const BASE_URL = 'https://pure-thicket-77677.herokuapp.com';
-export const BASE_URL = 'http://192.168.0.107:3000';
+export const BASE_URL = 'https://pure-thicket-77677.herokuapp.com';
+// export const BASE_URL = 'http://192.168.0.107:3000';
 export const whatever = 'whatever';
